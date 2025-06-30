018583ed684abc78f61042050c1b7f180a80d812
Dilip5561 <121309400+Dilip5561@users.noreply.github.com>
2025-06-30 15:40:19 +0530

    Update and rename LoginPage.java to App_Test(Login_test).java

--- App_Test(Login_test).java
+++ App_Test(Login_test).java
@@ -26,7 +26,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginPage {
+public class App_Test(Login_test) {
     WebDriver driver;
 
     @BeforeTest
